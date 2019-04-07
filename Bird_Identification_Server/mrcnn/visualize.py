a3fd2207799238577fe9c62e3efe2296c8f91df2
xxs <xixiangshu704@qq.com>
2019-04-07 10:27:03 +0800

    更新数据库数据，去除检测结果图像白边

--- Bird_Identification_Server/mrcnn/visualize.py
+++ Bird_Identification_Server/mrcnn/visualize.py
@@ -250,7 +250,17 @@ def save_detect_image_result(save_path,image, boxes, masks, class_ids, class_nam
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.savefig(save_path)
+
+    fig = plt.gcf()
+    fig.set_size_inches(7.0/3,7.0/3) #dpi = 300, output = 700*700 pixels
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    fig.savefig(save_path, format='jpg', transparent=True, dpi=300, pad_inches = 0)
+
+
+    #plt.savefig(save_path,dpi=300)
 
 def display_differences(image,
                         gt_box, gt_class_id, gt_mask,
--- Bird_Identification_Server/sql/bird_identification_app.sql
+++ Bird_Identification_Server/sql/bird_identification_app.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50713
 File Encoding         : 65001
 
-Date: 2019-04-07 10:02:36
+Date: 2019-04-07 10:26:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,5 +29,5 @@ CREATE TABLE `birds` (
 -- ----------------------------
 -- Records of birds
 -- ----------------------------
-INSERT INTO `birds` VALUES ('1', '黑脚信天翁', 'Black_footed_Albatross');
-INSERT INTO `birds` VALUES ('2', '黑背信天翁', 'Laysan_Albatross');
+INSERT INTO `birds` VALUES ('1', '黑脚信天翁,为鹱形目信天翁科下的一种大型海鸟， 单型种，无亚种分化。体长79-83厘米。嘴黑色，嘴基和眼下方灰白色，其余全身黑褐色。个别尾上覆羽和尾下覆羽白色，脚黑色，能与短尾信天翁区别。', 'Black_footed_Albatross');
+INSERT INTO `birds` VALUES ('2', '黑背信天翁,是一种中等体型的信天翁，81厘米长，体重2210-2800克，是一种有极长翼羽的大鸟，翅狭长，翼展195-215厘米。雄性略大。鼻孔侧位，不愈合。特征为自颏至臀部为全白色，但翼上及背深色。翼下主要为白色，具深色边缘，覆羽具近黑色纵纹。', 'Laysan_Albatross');
