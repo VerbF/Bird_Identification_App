aecc76660760f61abdfa6671a9feeac24a82bb26
V快 <xixiangshu704@qq.com>
2019-05-21 20:46:27 +0800

    补充完整 README.md ，更新 sql 脚本

--- Bird_Identification_Server/sql/bird_identification_app.sql
+++ Bird_Identification_Server/sql/bird_identification_app.sql
@@ -2,21 +2,20 @@
 Navicat MySQL Data Transfer
 
 Source Server         : xxs
-Source Server Version : 50713
+Source Server Version : 50722
 Source Host           : localhost:3306
 Source Database       : bird_identification_app
 
 Target Server Type    : MYSQL
-Target Server Version : 50713
+Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2019-04-14 14:09:06
+Date: 2019-05-21 19:31:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
--- Table structure for birds
+-- Table structure for `birds`
 -- ----------------------------
 DROP TABLE IF EXISTS `birds`;
 CREATE TABLE `birds` (
@@ -31,6 +30,6 @@ CREATE TABLE `birds` (
 -- ----------------------------
 INSERT INTO `birds` VALUES ('1', '黑脚信天翁,为鹱形目信天翁科下的一种大型海鸟， 单型种，无亚种分化。体长79-83厘米。嘴黑色，嘴基和眼下方灰白色，其余全身黑褐色。个别尾上覆羽和尾下覆羽白色，脚黑色，能与短尾信天翁区别。', 'Black_footed_Albatross');
 INSERT INTO `birds` VALUES ('2', '黑背信天翁,是一种中等体型的信天翁，81厘米长，体重2210-2800克，是一种有极长翼羽的大鸟，翅狭长，翼展195-215厘米。雄性略大。鼻孔侧位，不愈合。特征为自颏至臀部为全白色，但翼上及背深色。翼下主要为白色，具深色边缘，覆羽具近黑色纵纹。', 'Laysan_Albatross');
-INSERT INTO `birds` VALUES ('3', '乌信天翁,乌信天翁属鸟纲、鹱形目、信天翁科、乌信天翁属，是中型信天翁，栖息于南极，大部分时间飞翔在南大西洋和印度洋亚热带海域，主要食物是头足类动物，鱼类，甲壳动物和腐肉。', 'Sooty_Albatross');
-INSERT INTO `birds` VALUES ('4', '\r\n沟嘴犀鹃,属杜鹃科犀鹃属。全身黑色，分布于德克萨斯南部至秘鲁西部和巴西北部。', 'Groove_billed_Ani');
+INSERT INTO `birds` VALUES ('3', '靛蓝彩鹀是雀形目美洲雀科动物，生活在美国东半部。营巢于美国东半部分大片地区灌木丛，雄鸟几乎一身靛蓝色，但在墨西哥南部和中美洲越冬时会换上褐色的体羽，与雌鸟相仿。', 'Indigo_Bunting');
+INSERT INTO `birds` VALUES ('4', '沟嘴犀鹃,属杜鹃科犀鹃属。全身黑色，分布于德克萨斯南部至秘鲁西部和巴西北部。', 'Groove_billed_Ani');
 INSERT INTO `birds` VALUES ('5', '冠小海雀（Aethiacristatella）是海雀科的一种海鸟，身高约9英寸，羽毛为黑褐色或黑色，在繁殖季节，眼后会出现白色羽毛，头顶有一卷弯向前方的羽毛。喙为亮橙色，尾短，脚为黑色，有蹼。分部于阿拉斯加海岸地区，在开阔的海域过冬。以浮游生物为食。', 'Crested_Auklet');
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
+
 # Bird_Identification_App
+
 基于 Mask-Rcnn 的鸟类识别app
+
+## 服务端
+
+### 配置
+
+需要修改 Bird_Identification_Server  文件夹下的 server.py 中 ip_address 和 port 参数，其它保持不变。  
+
+```python
+#基础设置
+ip_address = '192.168.1.105'
+port = '8888'
+```
+
+另一个需要修改的是 Bird_Identification_Server 文件夹下的 detect.py 文件，需要对数据库的参数进行配置。
+
+```python
+#数据库设置
+DB_HOST = 'localhost'
+DB_PORT = 3306
+DB_USER_NAME = 'root'
+DB_PASSWORD = '704865074'
+DB_NAME = 'bird_identification_app'
+DB_CHARSET = 'utf8'
+```
+
+### 启动
+
+运行脚本 server.py ,博主使用了 Anaconda ，启动时只要在终端里输入  
+
+```python
+python server.py
+```
+
+## 用户端配置
+
+### 配置
+
+需要修改 Bird_Identification_App\app\src\main\java\com\example\xxs\bird_identification_app\ 路径下的 MainActivity.java 文件。  
+需要把这里的 url 改成和服务端的 ip_address , port 相对应。
+
+```python
+final String url = "http://192.168.1.105:8888/uploadImg";
+```
+
+## 数据库
+
+数据库使用 mysql ，在 Bird_Identification_Server\sql\ 路径下有 bird_identification_app.sql 脚本，需要先创建一个名为 bird_identification_app 的数据库（编码格式为 utf8），在该数据库下执行此脚本即可。
+
+## 注意
+
+- 使用路由器分配的 ip 地址时，要保证 安装 app 的手机和服务器接入同一局域网，否则无法使用。
+- 运行 server.py 和 detect.py 脚本时，可能会出现模块找不到的问题，需要安装相应的 python 库。
